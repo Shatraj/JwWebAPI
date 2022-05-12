cfca4a4f6c4e41f565b9085a06affcc274f9bccd
User <User@DESKTOP-7IDTPAJ>
2022-05-12 11:14:52 +0200

    Add project files.

---
+++ WebAPI/Controllers/SymmetricAlgorithmKey.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Controllers
+{
+    internal class SymmetricAlgorithmKey
+    {
+        private byte[] _Vs;
+
+        public SymmetricAlgorithmKey(byte[] vs)
+        {
+            _Vs = vs;
+        }
+    }
+}
---
+++ WebAPI/User.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
+    }
+}
